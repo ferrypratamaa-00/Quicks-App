d8537081fc247d36306d4f0ddefbb90bf91f893b
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2024-06-02 20:31:33 +0700

    feat: add getInitials function
    Create function to get initials from a name.

---
+++ src/utils/stringUtils.js
@@ -0,0 +1,6 @@
+export function getInitials(name) {
+    const words = name.split(" ");
+    return words.length > 1
+        ? `${words[0][0]}${words[words.length - 1][0]}`
+        : `${words[0][0]}`;
+}
