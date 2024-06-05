fbedde714c1741cb75a56bf7ba18e35a8ef37308
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2024-06-06 00:16:42 +0700

    bug/fix(Sidebar): fix open and close sidebar
    The sidebar makes absolute when open so as not to disturb the content and Navbar

--- src/components/templates/MainTemplate.jsx
+++ src/components/templates/MainTemplate.jsx
@@ -13,18 +13,25 @@ export default function MainTemplate({ children }) {
     return (
         <>
             <FloatingMenu />
-            <div className="h-screen bg-primary-dark">
-                <div className="flex flex-col sm:flex-row">
+            <div className="h-screen bg-primary-dark ">
+                <div className="flex flex-row">
                     <Sidebar
                         isOpen={openSidebar}
                         toggleSidebar={toggleSidebar}
+                        className={`${openSidebar && `absolute z-10`}`}
                     />
                     <div className="flex flex-col w-full">
                         <Navbar
                             isOpen={openSidebar}
                             toggleSidebar={toggleSidebar}
                         />
-                        <div className="p-4 text-white">{children}</div>
+                        <div
+                            className={`p-4 text-white ${
+                                openSidebar && "mt-10"
+                            }`}
+                        >
+                            {children}
+                        </div>
                     </div>
                 </div>
             </div>
